140f1bb41365550226367d665ed6d89c2e602dd1
Akhil Vijayan <vijayan.akhil.cet@gmail.com>
2020-09-11 22:48:25 -0400

    UI changes for performance

---
+++ assets/js/jquery/fontawesome.js
@@ -0,0 +1,51 @@
+<head>
+
+    <style>
+
+.iframe-container {    
+    padding-bottom: 60%;
+    padding-top: 30px; height: 0; overflow: hidden;
+}
+ 
+.iframe-container iframe,
+.iframe-container object,
+.iframe-container embed {
+    position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  /* spacing as needed */
+  padding: 20px 50px 20px 20px;
+
+  /* let it scroll */
+  overflow: auto;
+}
+
+    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <title>Teklrn</title>
+    <link rel="stylesheet" href="{% static 'css/cloudfare/font-awesome.min.css' %}" type="text/css" />
+    <link rel="stylesheet" href="{% static 'css/cloudfare/jquery-ui.css' %}" type="text/css" media="all"/>
+    
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="{% static 'css/stackpath.css' %}" type="text/css" />
+     <!-- Our Custom CSS -->
+    <link rel="stylesheet" href="{% static 'css/style1.css' %}" type="text/css" />
+    <link rel="stylesheet" href="{% static 'css/modal.css' %}" type="text/css" />
+    <link rel="stylesheet" href="{% static 'css/dist/attention.css' %}" type="text/css" />
+    <script src="{% static 'js/pdf/pdf.min.js' %}"></script>
+    <script type="text/javascript" src="{% static 'js/jquery/jquery.min.js' %}"></script>
+    <script type="text/javascript" src="{% static 'js/jquery/jquery.ui.min.js' %}"></script>
+    <script src="{% static 'js/dist/attention.js' %}"></script>
+    <script src="{% static 'js/login.js' %}"></script>
+    <script src="{% static 'js/modal.js' %}"></script>
+    
+    <!-- Font Awesome JS -->
+    <script defer src="{% static 'js/jquery/solid.js' %}" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+    <script defer src="{% static 'js/jquery/fontawesome.js' %}" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+</head>
