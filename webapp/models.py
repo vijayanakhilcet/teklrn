204a49d80e230706e254622aa87f8b0094ff0069
Akhil Vijayan <vijayan.akhil.cet@gmail.com>
2023-05-29 11:27:11 -0400

    Chnages to DailyNewsVideos Model

--- webapp/models.py
+++ webapp/models.py
@@ -204,5 +204,6 @@ class NewsSearchUrls(models.Model):
 class DailyNewsVideos(models.Model): 
     url = models.CharField(max_length=1000, default='')
     title = models.CharField(max_length=100, default='')
+    imgurl = models.CharField(max_length=1000, default='')
     def __str__(self):
         return self.url
