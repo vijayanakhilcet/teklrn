e822f32adefc48a9a0fda7707f0d50bc78b4205b
Akhil <vijayan.akhil.cet@gmail.com>
2023-10-11 16:28:47 -0400

    buy page changes

--- assets/new/js/checkout.js
+++ assets/new/js/checkout.js
@@ -20,7 +20,7 @@ function checkout(event){
                         
                         $.each(data, function(index) {
 
-                            htmlData+='<div class="wrap-input100 checkbox"><input checked type="checkbox" id="'+data[index].display+'" name="'+data[index].display+'" value="Bike"><label style="font-size: initial;" for="'+data[index].display+'">'+data[index].display+'</label><img src="'+data[index].imageUrl+'" alt="TEKLRN" width="100%" height="100"><div><div style="float: left; margin-top:10px;"><button style="background: black; border-radius: 5px;height: 15px;"  type="button" id="email_"  class="login100-form-btn"><a style="font-size: x-small;">Edit<a></button></div><div style="float: left; margin-left: 3px; margin-top:10px;"><button style="background: #3a69bb; border-radius: 5px;height: 15px;"  type="button" id="email_e" onclick="removeAdd(\''+data[index].display+'\')"   class="login100-form-btn"><a style="font-size: x-small;">Remove<a> </button></div></div></div>'
+                            htmlData+='<div class="wrap-input100 checkbox"><input style="margin-left: 12px;" checked type="checkbox" id="'+data[index].display+'" name="'+data[index].display+'" value="Bike"><label style="margin-left: 12px;font-size: initial;" for="'+data[index].display+'">'+data[index].display+'</label><img src="'+data[index].imageUrl+'" alt="TEKLRN" width="100%" height="200"><div><div style="float: left; margin-top:10px;"><button style="background: black; border-radius: 5px;height: 15px;"  type="button" id="email_"  class="login100-form-btn"><a style="font-size: x-small;">Edit<a></button></div><div style="float: left; margin-left: 3px; margin-top:10px;"><button style="background: #3a69bb; border-radius: 5px;height: 15px;"  type="button" id="email_e" onclick="removeAdd(\''+data[index].display+'\')"   class="login100-form-btn"><a style="font-size: x-small;">Remove<a> </button></div></div></div>'
                         
                         });
                         document.getElementById("checkout_list").innerHTML=htmlData;
@@ -44,15 +44,20 @@ function removeAdd(Ad){
             document.getElementById("checkout_list").innerHTML=''
             htmlData = ''
             
+            i=0
+
             $.each(data, function(index) {
             if(data[index].display=='NONE'){
                 window.open(window.location.origin+"/add_advertisements", "_self")
             }
 
-            htmlData+='<div class="wrap-input100 checkbox"><input checked type="checkbox" id="'+data[index].display+'" name="'+data[index].display+'" value="Bike"><label style="font-size: initial;" for="'+data[index].display+'">'+data[index].display+'</label><img src="'+data[index].imageUrl+'" alt="TEKLRN" width="100%" height="100"><div><div style="float: left; margin-top:10px;"><button style="background: black; border-radius: 5px;height: 15px;"  type="button" id="email_"  class="login100-form-btn"><a style="font-size: x-small;">Edit<a></button></div><div style="float: left; margin-left: 3px; margin-top:10px"><button style="background: #3a69bb; border-radius: 5px;height: 15px;"  type="button" id="email_e" onclick="removeAdd(\''+data[index].display+'\')"   class="login100-form-btn"><a style="font-size: x-small;">Remove<a> </button></div></div></div>'
-            
+            htmlData+='<div class="wrap-input100 checkbox"><input  style="margin-left: 12px;" checked type="checkbox" id="'+data[index].display+'" name="'+data[index].display+'" value="Bike"><label style="margin-left: 12px;font-size: initial;" for="'+data[index].display+'">'+data[index].display+'</label><img src="'+data[index].imageUrl+'" alt="TEKLRN" width="100%" height="200"><div><div style="float: left; margin-top:10px;"><button style="background: black; border-radius: 5px;height: 15px;"  type="button" id="email_"  class="login100-form-btn"><a style="font-size: x-small;">Edit<a></button></div><div style="float: left; margin-left: 3px; margin-top:10px"><button style="background: #3a69bb; border-radius: 5px;height: 15px;"  type="button" id="email_e" onclick="removeAdd(\''+data[index].display+'\')"   class="login100-form-btn"><a style="font-size: x-small;">Remove<a> </button></div></div></div>'
+            i=i+1
             });
             document.getElementById("checkout_list").innerHTML=htmlData;
+            document.getElementById("items").value = i
+            document.getElementById("amount").value = 13.0*i
+
 });
 
 event.preventDefault();
