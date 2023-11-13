2e41a528902474ec31d3014883d30a73608f526b
Akhil <vijayan.akhil.cet@gmail.com>
2023-11-13 16:04:31 -0500

    Ads

--- assets/new/js/news.js
+++ assets/new/js/news.js
@@ -87,7 +87,7 @@ getNewsContent();
          document.getElementById("para-ins").innerHTML = data[index].para
         });
         document.getElementById("forAd").innerHTML = document.getElementById("ads").innerHTML
-      
+        document.getElementById("ads").innerHTML=''
    })
    .complete(function(data) {
           $.ajax({
@@ -107,14 +107,20 @@ getNewsContent();
              
                li_el += '<li><span class="tree_label"><div onclick="gotoTechnology1(\''+data[index].newtitle+'\',\''+data[index].newtitle+'\',\''+'RelatedNws'+i+'---'+data[index].newtitle+'\')" class="col-md-6"><p style="pointer-events: none; object-fit: cover;"><img style="border-radius: 11px;width: 100%; height: 250px; pointer-events: none; object-fit: cover;" id="RelatedNws'+i+'---'+data[index].newtitle+'" onerror="this.src=\'/static/image/test/certificate.jpg\'" src="/static/image/test/certificate.jpg" width="100%" height="30px" alt="Image" class="img-fluid"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+data[index].newtitle+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
               
-               if (i=2 || i==5 || i==8 || i==10){
-                  li_el += '<li><span class="tree_label"></span><div class="col-md-6"><p style="object-fit: cover;"><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3671375601838095" crossorigin="anonymous"></script><ins class="adsbygoogle" style="display:block; text-align:center;" data-ad-layout="in-article"   data-ad-format="fluid"  data-ad-client="ca-pub-3671375601838095"  data-ad-slot="7874188453"></ins><script>     (adsbygoogle = window.adsbygoogle || []).push({});</script></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: "Poppins", sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;"></a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></div></div></div></span></li>';
+               if (i==2 || i==5 || i==8 || i==10){
+                  li_el += '<li><span class="tree_label"></span><div class="col-md-6"><p id="ad-p-"'+i+' style="object-fit: cover;"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: "Poppins", sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;"></a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></div></div></div></span></li>';
 
 
                }
               }); 
               document.getElementById("tree").innerHTML = "";
               document.getElementById("tree").innerHTML = li_el;
+              document.getElementById("ad-p-2").innerHTML = document.getElementById("forAd").innerHTML
+              document.getElementById("ad-p-5").innerHTML = document.getElementById("forAd").innerHTML
+              document.getElementById("ad-p-8").innerHTML = document.getElementById("forAd").innerHTML
+              document.getElementById("ad-p-10").innerHTML = document.getElementById("forAd").innerHTML
+
+
               
          })
          .complete(function(data) {
