db24f58a791dff9440f96b9a88ab12523659a375
Akhil <vijayan.akhil.cet@gmail.com>
2023-11-23 16:46:55 -0500

    News Search

--- webapp/views.py
+++ webapp/views.py
@@ -1746,38 +1746,51 @@ class MatchingTheSearchNewView(FormView):
         results= []
         data = request.GET
         searchData = data.get("srch").replace(" ", "+")
-        print(searchData)
-        # c = data.get("search_string")
-        # l = data.get("lang")
-        # idx = data.get("idx")  
+        headers = {
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"
+        }
         contentType= 'Search Results: '+data.get("srch")
-        url  = "https://search.yahoo.com/search?p="+searchData
-        print(url)
-        r = requests.get(url)
+        r = requests.get('https://apnews.com/search?q='+searchData, headers=headers)
         soup = BeautifulSoup(r.content)
-        for a in soup.find_all('a'):
-            if len(a.text.strip().split(' '))>5 and 'www.' not in a.text and '›' not in a.text and 'See All' not in a.text:
+        for a in soup.find_all('span', {'class' : 'PagePromoContentIcons-text'}):    
+            if len(a.text.strip().split(' '))>6:
                 course_json = {}
-                # img = '/static/image/test/certificate.jpg'
-                course_json['technology'] = ' '.join(a.text.split()).replace("'", "").replace("‘", "").replace("\"", "").replace("’", "").replace(",", " ").replace(":", " ").replace("opinion content.", "").replace("review.", "").replace("video content.", "").replace("tech tonic.", "").strip()
+                course_json['technology'] = a.text.strip()
                 course_json['description'] =  course_json['technology']                 
                 course_json['contentType'] = contentType
                 course_json['count'] = 9999
                 results.append(course_json)
-        # results=[]
         if not results:
-            for a in soup.find_all('span'):
-                # if len(a.text.strip().split(' '))>4 and 'www.' not in a.text and '›' not in a.text:
-                if len(a.text.strip().split(' '))>5 and 'www.' not in a.text and '›' not in a.text:
+            url  = "https://search.yahoo.com/search?p="+searchData
+            print(url)
+            r = requests.get(url, headers=headers)
+            soup = BeautifulSoup(r.content)
+            for a in soup.find_all('a'):
+                if len(a.text.strip().split(' '))>5 and 'www.' not in a.text and '›' not in a.text and 'See All' not in a.text:
                     course_json = {}
                     # img = '/static/image/test/certificate.jpg'
                     course_json['technology'] = ' '.join(a.text.split()).replace("'", "").replace("‘", "").replace("\"", "").replace("’", "").replace(",", " ").replace(":", " ").replace("opinion content.", "").replace("review.", "").replace("video content.", "").replace("tech tonic.", "").strip()
-                    course_json['description'] =  course_json['technology'] 
-                    if len(course_json['description'].split(" "))<5:
-                        continue 
+                    course_json['description'] =  course_json['technology']                 
                     course_json['contentType'] = contentType
                     course_json['count'] = 9999
                     results.append(course_json)
+            # results=[]
+            if not results:
+                url  = "https://search.yahoo.com/search?p="+searchData
+                r = requests.get(url, headers=headers)
+                for a in soup.find_all('span'):
+                    # if len(a.text.strip().split(' '))>4 and 'www.' not in a.text and '›' not in a.text:
+                    if len(a.text.strip().split(' '))>5 and 'www.' not in a.text and '›' not in a.text:
+                        course_json = {}
+                        # img = '/static/image/test/certificate.jpg'
+                        course_json['technology'] = ' '.join(a.text.split()).replace("'", "").replace("‘", "").replace("\"", "").replace("’", "").replace(",", " ").replace(":", " ").replace("opinion content.", "").replace("review.", "").replace("video content.", "").replace("tech tonic.", "").strip()
+                        course_json['description'] =  course_json['technology'] 
+                        if len(course_json['description'].split(" "))<5:
+                            continue 
+                        course_json['contentType'] = contentType
+                        course_json['count'] = 9999
+                        results.append(course_json)
 
         random.shuffle(results)
         print(results)
