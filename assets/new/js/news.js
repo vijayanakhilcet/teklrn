0d7273ae9fa092a155d2fbca606832e98bf177b9
Akhil <vijayan.akhil.cet@gmail.com>
2023-12-15 12:41:19 -0500

    Performance improvements

--- assets/new/js/news.js
+++ assets/new/js/news.js
@@ -98,85 +98,86 @@ getNewsContent();
             }
    })
    .complete(function(data) {
-          $.ajax({
-              url: "/getRelatedNews", // the url where we want to POST
-              data: {
-              "titles": document.getElementById("technology_view").textContent
-              },
-              dataType: 'json',
-              encode: true
-          })
-          .done(function (data) {
-           
-            li_el = "";
-            i=0
-            $.each(data, function (index) {
-               i=i+1;
-             
-               li_el += '<li><span class="tree_label"><div onclick="gotoTechnology1(\''+data[index].newtitle+'\',\''+data[index].newtitle+'\',\''+'RelatedNws'+i+'---'+data[index].newtitle+'\')" class="col-md-6"><p style="pointer-events: none; object-fit: cover;"><img style="border-radius: 11px;width: 100%; height: 250px; pointer-events: none; object-fit: cover;" id="RelatedNws'+i+'---'+data[index].newtitle+'" onerror="this.src=\'/static/image/test/certificate.jpg\'" src="/static/image/test/certificate.jpg" width="100%" height="30px" alt="Image" class="img-fluid"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+data[index].newtitle+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
-              
-               if (i%3==0){
-                  li_el += '<li><span class="tree_label"><div class="col-md-6"><p id="ad-p-'+i+'" style="object-fit: cover !important; overflow-clip-margin: content-box; overflow: clip;"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+''+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
+          
+   });
+   $.ajax({
+      url: "/getRelatedNews", // the url where we want to POST
+      data: {
+      "titles": document.getElementById("technology_view").textContent
+      },
+      dataType: 'json',
+      encode: true
+  })
+  .done(function (data) {
+   
+    li_el = "";
+    i=0
+    $.each(data, function (index) {
+       i=i+1;
+     
+       li_el += '<li><span class="tree_label"><div onclick="gotoTechnology1(\''+data[index].newtitle+'\',\''+data[index].newtitle+'\',\''+'RelatedNws'+i+'---'+data[index].newtitle+'\')" class="col-md-6"><p style="pointer-events: none; object-fit: cover;"><img style="border-radius: 11px;width: 100%; height: 250px; pointer-events: none; object-fit: cover;" id="RelatedNws'+i+'---'+data[index].newtitle+'" onerror="this.src=\'/static/image/test/certificate.jpg\'" src="/static/image/test/certificate.jpg" width="100%" height="30px" alt="Image" class="img-fluid"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+data[index].newtitle+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
+      
+       if (i%3==0){
+          li_el += '<li><span class="tree_label"><div class="col-md-6"><p id="ad-p-'+i+'" style="object-fit: cover !important; overflow-clip-margin: content-box; overflow: clip;"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+''+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
 
-               }
-              }); 
-              document.getElementById("tree").innerHTML = "";
-              document.getElementById("tree").innerHTML = li_el;
-              try {               
-             
-                  document.getElementById("ad-p-3").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-6").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-9").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-12").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-15").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-18").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-21").innerHTML = document.getElementById("copy-ads").innerHTML
-                  document.getElementById("ad-p-24").innerHTML = document.getElementById("copy-ads").innerHTML
+       }
+      }); 
+      document.getElementById("tree").innerHTML = "";
+      document.getElementById("tree").innerHTML = li_el;
+      try {               
+     
+          document.getElementById("ad-p-3").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-6").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-9").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-12").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-15").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-18").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-21").innerHTML = document.getElementById("copy-ads").innerHTML
+          document.getElementById("ad-p-24").innerHTML = document.getElementById("copy-ads").innerHTML
 
-                  document.getElementById("ads").remove();
-                  document.getElementById("li-copy-ads").remove();
+          document.getElementById("ads").remove();
+          document.getElementById("li-copy-ads").remove();
 
-               }
-               catch(err) {
-                  document.getElementById("ads").remove();
-                  document.getElementById("li-copy-ads").remove();
-               }
-              
-         })
-         .complete(function(data) {
-                var datas = document.querySelectorAll('[id^="'+'RelatedNws'+'"]');
-                // Code = $("#countryCode option:selected").val().split("---")[0];
-                data = ''
-                datas.forEach((userItem) => {
-                    data=userItem.id;
-                    $.ajax({
-                        url: "/searchtopicsnewnewsForImgRelated", // the url where we want to POST
-                        data: {
-                        "strVal": data.split('---')[1],
-                        "titles": data,
-                        "lang":'en'
-                        
-                        },
-                        dataType: 'json',
-                        encode: true
-                    })
-                    .done(function (data) {
-                     $.each(data, function (index) {
-                       var elm = document.getElementById(data[index].title);
-                       elm.src = data[index].src;
-                       });
-                       
-                          });
-                         
-                       
-                  });
-                          populateStaticNews("zzz");
-                          // populateCountry();   
-                          refineSearchView1("zzz", 'en', -1);
+       }
+       catch(err) {
+          document.getElementById("ads").remove();
+          document.getElementById("li-copy-ads").remove();
+       }
+      
+ })
+ .complete(function(data) {
+        var datas = document.querySelectorAll('[id^="'+'RelatedNws'+'"]');
+        // Code = $("#countryCode option:selected").val().split("---")[0];
+        data = ''
+        datas.forEach((userItem) => {
+            data=userItem.id;
+            $.ajax({
+                url: "/searchtopicsnewnewsForImgRelated", // the url where we want to POST
+                data: {
+                "strVal": data.split('---')[1],
+                "titles": data,
+                "lang":'en'
+                
+                },
+                dataType: 'json',
+                encode: true
+            })
+            .done(function (data) {
+             $.each(data, function (index) {
+               var elm = document.getElementById(data[index].title);
+               elm.src = data[index].src;
                });
                
-                       document.getElementById("scan_clicker").click();
-   });
+                  });
+                 
+               
+          });
+                  populateStaticNews("zzz");
+                  // populateCountry();   
+                  refineSearchView1("zzz", 'en', -1);
+       });
+       
+               document.getElementById("scan_clicker").click();
 
 event.preventDefault();
  }
