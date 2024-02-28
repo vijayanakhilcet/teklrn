1c65ce5aa013b83d36005546f49b31b2d477f614
Akhil <vijayan.akhil.cet@gmail.com>
2024-02-28 18:34:36 -0500

    UI Improvements

--- assets/new/js/news.js
+++ assets/new/js/news.js
@@ -135,7 +135,7 @@ $(function () {
        li_el += '<li><span class="tree_label"><div onclick="gotoTechnology1(\''+data[index].newtitle+'\',\''+data[index].newtitle+'\',\''+'RelatedNws'+i+'---'+data[index].newtitle+'\')" class="col-md-6"><p style="pointer-events: none; object-fit: cover;"><img style="border-radius: 11px;width: 100%; height: 250px; pointer-events: none; object-fit: cover;" id="RelatedNws'+i+'---'+data[index].newtitle+'" onerror="this.src=\'/static/image/test/certificate.jpg\'" src="/static/image/test/certificate.jpg" width="100%" height="30px" alt="Image" class="img-fluid"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+data[index].newtitle+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
       
        if (i%3==0){
-          li_el += '<li id="ad-p-'+i+'"><span class="tree_label"><div class="col-md-6"><p id="ad-ppppp-'+i+'" style="width: 377px !important;object-fit: cover !important; overflow-clip-margin: content-box; overflow: clip;"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+''+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
+          li_el += '<li><span class="tree_label"><div class="col-md-6"><p id="ad-p-'+i+'" style="width: 377px !important;object-fit: cover !important; overflow-clip-margin: content-box; overflow: clip;"></p><div class="d-flex post-entry"><div class="post-content"><div style="text-transform: capitalize;font-weight: 450 !important; font-family: \'Poppins\', sans-serif; font-size: 13px !important;  color:black !important"><a style="padding-left: 0px !important;background: none; font-weight: 450px;">'+''+'</a><img src="/static/image/images/read_b.png" style="width: 20%;float: right;"></div><hr><p style="font-size:13px;display: block; text-overflow: ellipsis;  word-wrap: break-word;  overflow: hidden;  max-height: 3.6em;  line-height: 1.8em;"> </p><div class="post-meta"> </div><div class="post-meta"></div></h4></div></div></div></span></li>';
           
           test_html_message +='<div style=""><p id="ad-pp-'+i+'" style="padding-left:2%;font-size:xx-small;position:absolute; color:white;font-weight:600;bottom: 2px; text-transform: uppercase;border-bottom: 1mm ridge #1c8ccd;z-index:31222; width:100%; text-overflow: clip; overflow: hidden; "></p></div>';
           test_html_message_mgc3 +='<div style=""><p id="ad-ppp-'+i+'" style="padding-left:2%;font-size:xx-small;position:absolute; color:white;font-weight:600;bottom: 2px; text-transform: uppercase;border-bottom: 1mm ridge #1c8ccd;z-index:31222; width:100%; text-overflow: clip; overflow: hidden; "></p></div>';
@@ -149,35 +149,35 @@ $(function () {
       document.getElementById("mgc3").innerHTML = test_html_message_mgc3;
       try {               
      
-          document.getElementById("ad-p-3").innerHTML =  document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-3").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-3").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-3").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-6").innerHTML =  document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-6").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-6").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-6").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-9").innerHTML =  document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-9").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-9").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-9").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-12").innerHTML =  document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-12").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-12").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-12").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-15").innerHTML = document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-15").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-15").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-15").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-18").innerHTML = document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-18").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-18").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-18").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-21").innerHTML = document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-21").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-21").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-21").innerHTML = document.getElementById("ads").innerHTML
 
-          document.getElementById("ad-p-24").innerHTML = document.getElementById("li-copy-ads").innerHTML
+          document.getElementById("ad-p-24").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-pp-24").innerHTML = document.getElementById("ads").innerHTML
           document.getElementById("ad-ppp-24").innerHTML = document.getElementById("ads").innerHTML
 
